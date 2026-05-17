b419704d8a7811379beeb1882d74c70c67c5ebb0
Taras-Lisnyak <taras4lisnyak@gmail.com>
2026-05-17 17:25:02 +0300

    edit carcard and add var

--- components/ui/CarCard.tsx
+++ components/ui/CarCard.tsx
@@ -8,22 +8,32 @@ export default function CarCard({ car }: { car: Car }) {
 
   return (
     <div className={styles.card}>
-      {car.img && (
-        <Image
-  src={car.img}
-  alt={`${car.brand} ${car.model}`}
-  width={200}   
-  height={120}  
-  className={styles.image}
-/>
-
-      )}
-      <h3 className={styles.title}>
-        {car.brand} {car.model}
-      </h3>
-      <p className={styles.text}>Price: ${car.rentalPrice}</p>
-      <p className={styles.text}>Mileage: {car.mileage} km</p>
-      <Link href={`/catalog/${car.id}`} target="_blank" className={styles.link}>
+      <div className={styles.cardContent}>
+        {car.img && (
+          <Image
+          src={car.img}
+          alt={`${car.brand} ${car.model}`}
+          width={200}   
+          height={120}  
+          className={styles.image}
+        />
+        )}
+        <div className={styles.cardInfo}>
+          <h3 className={styles.title}>
+            {car.brand} <span className={styles.model}>{car.model}</span>, {car.year}
+          </h3>
+          <p className={styles.price}>${car.rentalPrice}</p>
+          <div className={styles.locationDetails}>
+            <p className={styles.location}>
+              {car.location.city}, {car.location.country} | {car.rentalCompany}
+            </p>
+            <p className={styles.details}>
+              {car.type} | {car.mileage} km
+            </p>
+          </div>
+        </div>
+      </div>
+      <Link href={`/catalog/${car.id}`} target="_blank" className={styles.buttonLink}>
         Read more
       </Link>
     </div>
